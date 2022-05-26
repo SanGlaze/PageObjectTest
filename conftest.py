9a675d8e840d37a0831badaee7135ea8c28ef885
SanGlaze <87697531+SanGlaze@users.noreply.github.com>
2022-05-26 21:17:36 +0600

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -16,6 +16,6 @@ def browser(request):
     options.add_experimental_option('prefs', {'intl.accept_languages': users_language})
     browser = webdriver.Chrome(options=options)
     
-    time.sleep(10)
+    browser.implicitly_wait(5)
     yield browser
     browser.quit()
